336e951a42e4b9fa43cf9f655323d11db8987567
back2dos <back2dos@googlemail.com>
2015-11-25 21:43:26 +0100

    Disable substitution with private classes for now.

--- src/single_impl/Sweep.hx
+++ src/single_impl/Sweep.hx
@@ -10,11 +10,12 @@ class Sweep {
   static function use() {
     Context.onGenerate(function (types:Array<Type>) {
       for (x in types.getSingleImplementations()) {
-        if (!x.orphan.meta.has(':remove')) {
-          x.orphan.meta.add(':remove', [], x.impl.pos);
-          x.orphan.meta.add(':native', [macro $v{x.impl.nameOf()}], x.impl.pos);
-          x.orphan.exclude();
-        }
+        if (!x.impl.isPrivate)
+          if (!x.orphan.meta.has(':remove')) {
+            x.orphan.meta.add(':remove', [], x.impl.pos);
+            x.orphan.meta.add(':native', [macro $v{x.impl.nameOf()}], x.impl.pos);
+            x.orphan.exclude();
+          }
       }
     });
   }
